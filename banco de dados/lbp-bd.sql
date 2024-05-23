5863f751b9991ada5d5fe9ca0eb17875f5eca05c
Henrique <henrique.souza@sptech.school>
2024-05-23 10:25:00 -0300

    subindo dnv o bd

--- banco de dados/lbp-bd.sql
+++ banco de dados/lbp-bd.sql
@@ -1,3 +1,19 @@
 create database lbp;
 use lbp;
 
+-- Tabela de usuários
+CREATE TABLE Usuario (
+    idUser INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) not null,
+    email VARCHAR(100) UNIQUE,
+    senha VARCHAR(100) not null
+);
+
+-- Tabela de mensagens
+CREATE TABLE Mensagem (
+    idMensagem INT PRIMARY KEY AUTO_INCREMENT,
+    fkUser INT,
+    Mensagem VARCHAR(600),
+    DataCriacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (fkUser) REFERENCES Usuarios(idUser)
+);
